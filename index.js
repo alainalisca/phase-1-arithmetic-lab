fde15d75642dd2bb65fda81dd65ac02be5151dce
Alain Alisca <alainalisca@gmail.com>
2022-02-24 06:09:50 -0500

    commit tree 4a6a022a1a8ddd27ef63b93330e1db356a1a13ad

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply; 
+let num1 = 31; 
+let num2 = 2; 
 
-// Write your code here
+multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 2 + 120);
+
+let mod; 
+let num3 = 64; 
+let num4 = 10; 
+mod = num3 % num4; 
+
+let max = Math.max(12, 10, 9, 15, 20, 19); 
+
+
